d63d271d8dc2bc9e1be0884b509d884b9c4fd5cd
gun <99672136+kanruethaiii@users.noreply.github.com>
2025-03-11 21:11:20 +0700

    aa

--- services/cat.js
+++ services/cat.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const base_url = "https://catshopfinal.proen.app.ruk-com.cloud";
+const base_url = "https://catshopfinal.proen.app.ruk-com.cloud/";
 const CatService = {
   getAll: async () => {
     return await axios.get(`${base_url}/cats`);
